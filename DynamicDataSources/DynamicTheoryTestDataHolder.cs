bcd7f7a731568414aa749bb8530e6aeb66a6dbef
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-11-10 08:13:12 +0100

    namespace correction

--- DynamicDataSources/DynamicTheoryTestDataHolder.cs
+++ DynamicDataSources/DynamicTheoryTestDataHolder.cs
@@ -1,7 +1,7 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
-namespace CsabaDu.DynamicTestData.xUnit.v3.DynamicDataRowSources;
+namespace CsabaDu.DynamicTestData.Lite.xUnit.v3.DynamicDataSources;
 
 public abstract class DynamicTheoryTestDataHolder(ArgsCode argsCode, PropsCode propsCode)
 : DynamicDataSource<ITheoryTestData>(argsCode, propsCode)
