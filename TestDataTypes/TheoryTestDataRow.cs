d74286329a4f25eb45e289edabb775816fb5f15a
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-11-11 08:31:34 +0100

    DataTypes namespace renamed to TestDataTypes

--- TestDataTypes/TheoryTestDataRow.cs
+++ TestDataTypes/TheoryTestDataRow.cs
@@ -1,7 +1,7 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
-namespace CsabaDu.DynamicTestData.Lite.xUnit.v3.DataTypes;
+namespace CsabaDu.DynamicTestData.Lite.xUnit.v3.TestDataTypes;
 
 public sealed class TheoryTestDataRow<TTestData>(
     TTestData testData,
